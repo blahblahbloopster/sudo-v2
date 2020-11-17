bf687667fbfa7298ebf6cfdc022c5222e6737f5e
Max Leibowitz <403089@bsd48.org>
2020-11-17 14:56:10 -0800

    Fixed build.gradle and working on basics

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,10 @@
+import net.dv8tion.jda.api.JDABuilder
+import net.dv8tion.jda.api.entities.Activity
 import java.io.File
 
 fun main() {
-    val token = File("token");
-    println(token.readText())
+    val token = File("token").readText()
+    val builder = JDABuilder.createDefault(token)
+    builder.setActivity(Activity.watching("for sudo help"))
+    val jda = builder.build()
 }
