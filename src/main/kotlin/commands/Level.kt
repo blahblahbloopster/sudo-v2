da420eb5ca8dc8f79ae4febd4f739d9562ae7a93
Max Leibowitz <403089@bsd48.org>
2020-11-18 15:34:00 -0800

    Changed level to use the member's color

--- src/main/kotlin/commands/Level.kt
+++ src/main/kotlin/commands/Level.kt
@@ -5,8 +5,6 @@ import EmbedResponse
 import Sendable
 import net.dv8tion.jda.api.EmbedBuilder
 import net.dv8tion.jda.api.entities.Message
-import java.awt.Color
-import java.util.*
 import kotlin.math.ceil
 import kotlin.math.roundToInt
 
@@ -17,8 +15,8 @@ class Level : Command() {
     override fun process(args: List<String>, message: Message): Sendable {
         val embed = EmbedBuilder()
         val width = 30
-        val user = if (message.mentionedMembers.isNotEmpty()) message.mentionedMembers[0].user else message.author
-        val level = Points.getLevel(user.id)
+        val member = if (message.mentionedMembers.isNotEmpty()) message.mentionedMembers[0] else message.member!!
+        val level = Points.getLevel(member.id)
         val xp = Points.levelToXp(level)
 
         val endLevelXp = Points.levelToXp(ceil(level))
@@ -31,8 +29,8 @@ class Level : Command() {
         embed.addField("Level", level.toInt().toString(), true)
         embed.addField("Xp", "$xp / $endLevelXp", true)
         embed.addField("Progress", "`|${chars.last().toString().repeat(fullBars)}${chars[partialBar]}${" ".repeat((width - 1) - fullBars)}|`", false)
-        embed.setColor(Color.getHSBColor(Random(user.idLong).nextFloat(), 0.8f, 0.8f))
-        embed.setThumbnail(user.avatarUrl)
+        embed.setColor(member.color)
+        embed.setThumbnail(member.user.avatarUrl)
         return EmbedResponse(embed)
     }
 }
